e1990e853c8b2ac3752a1ebec26653216a1ce190
Anvar Hunt <anvar.hunt@gmail.com>
2019-03-28 23:11:19 +0600

    Added Entities, qr gen, get Attendances

---
+++ backend/src/main/java/com/myspring/entities/JsonResponse.java
@@ -0,0 +1,12 @@
+package com.myspring.entities;
+
+public class JsonResponse {
+    boolean success;
+
+    public JsonResponse() {
+    }
+
+    public JsonResponse(boolean success) {
+        this.success = success;
+    }
+}
---
+++ backend/src/main/java/com/myspring/entities/TransitUser.java
@@ -0,0 +1,67 @@
+package com.myspring.entities;
+
+public class TransitUser {
+    private String login;
+    private String password;
+    private int role;
+    private String firstName;
+    private String lastName;
+    private String groupName;
+
+    public TransitUser(String login, String password, int role, String firstName, String lastName, String groupName) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.groupName = groupName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+}
